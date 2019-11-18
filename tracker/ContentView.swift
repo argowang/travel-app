d499388a1cc45171ad608b8829c2f54e284ed873
Bingxin Zhu <bingxin@fb.com>
2019-11-17 21:43:06 -0800

    temp map with current location

--- tracker/ContentView.swift
+++ tracker/ContentView.swift
@@ -8,8 +8,9 @@
 
 import SwiftUI
 import CoreData
+import MapKit
 
-struct ContentView: View {
+struct ContentViews: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     @ObservedObject var locationManager = LocationManager()
     @FetchRequest(fetchRequest: Record.allRecordsFetchRequest()) var records: FetchedResults<Record>
@@ -35,7 +36,7 @@ struct ContentView: View {
             Section(header: Text("Add Visited Place")) {
                 VStack {
                     VStack {
-                        TextField("Location", text: self.$newLocation)
+                        TextField("Name Currrent Visiting Location", text: self.$newLocation)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
                     
@@ -110,9 +111,93 @@ struct ContentView: View {
     }
 }
 
+struct MapView : UIViewRepresentable {
+     
+    @Binding var manager : CLLocationManager
+    @Binding var alert : Bool
+    let map = MKMapView()
+    
+    func makeCoordinator() -> MapView.Coordinator {
+        return Coordinator(parent1: self)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        let center = CLLocationCoordinate2D(latitude: 13.086, longitude: 80.2707)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        map.region = region
+        manager.requestWhenInUseAuthorization()
+        manager.delegate = context.coordinator
+        manager.startUpdatingLocation()
+        return map
+    }
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        
+    }
+    
+    class Coordinator : NSObject,CLLocationManagerDelegate{
+        
+        var parent : MapView
+        
+        init(parent1 : MapView) {
+            
+            parent = parent1
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            
+            if status == .denied{
+                
+                parent.alert.toggle()
+            }
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            
+            let location = locations.last
+            let point = MKPointAnnotation()
+            
+            let georeader = CLGeocoder()
+            georeader.reverseGeocodeLocation(location!) { (places, err) in
+                
+                if err != nil{
+                    
+                    print((err?.localizedDescription)!)
+                    return
+                }
+                
+                let place = places?.first?.locality
+                point.title = place
+                point.subtitle = "Current"
+                point.coordinate = location!.coordinate
+                self.parent.map.removeAnnotations(self.parent.map.annotations)
+                self.parent.map.addAnnotation(point)
+                
+                let region = MKCoordinateRegion(center: location!.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                self.parent.map.region = region
+            }
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @State var manager = CLLocationManager()
+    @State var alert = false
+    
+    var body: some View {
+        VStack{
+            // ℹ️ need to restruct later
+            MapView(manager: $manager, alert: $alert).alert(isPresented: $alert) {
+                Alert(title: Text("Please Enable Location Access In Settings Pannel !!!"))
+            }
+            ContentViews()
+                     .environment(\.managedObjectContext, (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .environment(\.managedObjectContext, (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
     }
 }
