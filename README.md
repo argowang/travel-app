383593d68747382c2e684d60dbb839dce9c68c0b
Junyi (Argo) Wang <wangjunyi@hotmail.com>
2019-11-17 22:36:40 -0800

    Apply swift format (#2)
    * apply swift format
    
    * add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+### Development Guide
+Please ensure you install and apply the following tools before raising PR
+
+[Formatter](https://github.com/nicklockwood/SwiftFormat#config-file)
--- tracker/LocationManager.swift
+++ tracker/LocationManager.swift
@@ -6,37 +6,36 @@
 //  Copyright © 2019 TechLead. All rights reserved.
 //
 
-import Foundation
-import CoreLocation
 import Combine
+import CoreLocation
+import Foundation
 
 class LocationManager: NSObject, ObservableObject {
-    
     override init() {
         super.init()
-        self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-    
+
     @Published var locationStatus: CLAuthorizationStatus? {
         willSet {
             objectWillChange.send()
         }
     }
-    
+
     @Published var lastLocation: CLLocation? {
         willSet {
             objectWillChange.send()
         }
     }
-    
+
     var statusString: String {
         guard let status = locationStatus else {
             return "unknown"
         }
-        
+
         switch status {
         case .notDetermined: return "notDetermined"
         case .authorizedWhenInUse: return "authorizedWhenInUse"
@@ -45,25 +44,22 @@ class LocationManager: NSObject, ObservableObject {
         case .denied: return "denied"
         default: return "unknown"
         }
-        
     }
-    
+
     let objectWillChange = PassthroughSubject<Void, Never>()
-    
+
     private let locationManager = CLLocationManager()
 }
 
 extension LocationManager: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        self.locationStatus = status
+    func locationManager(_: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
         print(#function, statusString)
     }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+    func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
-        self.lastLocation = location
+        lastLocation = location
         print(#function, location)
     }
-    
 }
