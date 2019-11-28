75c538528fdc4dadc00324ab70bedaf53a8ae5b5
Bingxin Zhu <bingxin@fb.com>
2019-11-28 11:47:18 -0800

    change schema

---
+++ tracker/Core Data/TripCard.swift
@@ -0,0 +1,24 @@
+//
+//  TripCard.swift
+//  tracker
+//
+//  Created by Bingxin Zhu on 11/28/19.
+//  Copyright © 2019 TechLead. All rights reserved.
+//
+ 
+import CoreData
+import Foundation
+
+public class TripCard: NSManagedObject, Identifiable {
+    @NSManaged public var title: String?
+    @NSManaged public var start: Date?
+    @NSManaged public var end: Date?
+}
+
+extension TripCard {
+    static func allTripCardsFetchRequest() -> NSFetchRequest<TripCard> {
+        let request: NSFetchRequest<TripCard> = NSFetchRequest<TripCard>(entityName: "TripCard")
+        request.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
+        return request
+    }
+}
