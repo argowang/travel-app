6a639d4a51e5ba43675d63ac940fda65f78edd6d
Bingxin Zhu <bingxin@fb.com>
2019-11-30 15:24:40 -0800

    refactor

--- tracker/Views/CardListView.swift
+++ tracker/Views/CardListView.swift
@@ -21,23 +21,23 @@ struct CardListView: View {
     @State var title = ""
 
     var body: some View {
-            ScrollView {
-                VStack {
-                    ForEach(self.tripCards) { card in
-                        NavigationLink(destination: ViewTripEventInfoView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))) {
-                            CardView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))
-                        }
-                        .buttonStyle(PlainButtonStyle())
+        ScrollView {
+            VStack {
+                ForEach(self.tripCards) { card in
+                    NavigationLink(destination: ViewTripEventInfoView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))) {
+                        CardView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))
                     }
+                    .buttonStyle(PlainButtonStyle())
                 }
-                Button(action: {
-                    if self.title != "" {}
-                }) {
-                    NavigationLink(destination: AddTripEventInfoView()) {
-                        Text("Add event")
-                    }
+            }
+            Button(action: {
+                if self.title != "" {}
+            }) {
+                NavigationLink(destination: AddTripEventInfoView()) {
+                    Text("Add event")
                 }
-            } 
+            }
+        }
     }
 }
 
