6144e98c6d1101fcb9ae3fad1ad586a97d6498dc
Ergou <junyi.wang@columbia.edu>
2019-12-02 22:18:04 -0800

    location is saved only if user clicked save

--- tracker/Views/Location Views/SetCurrentLocationView.swift
+++ tracker/Views/Location Views/SetCurrentLocationView.swift
@@ -13,25 +13,33 @@ struct SetCurrentLocationView: View {
     @Binding var newLocation: String
     @Binding var selectedCoordinate: CLLocationCoordinate2D?
 
+    @State var draftNewLocation: String = ""
+    @State var draftSelectedCoordinate: CLLocationCoordinate2D?
     @State var alert = false
     @State var nearByPlaces: [MKMapItem] = []
     @State var cardPosition = CardPosition.middle
 
-//    @EnvironmentObject var manager: LocationManager
     @EnvironmentObject var placeFinder: PlaceFinder
+
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
 
     var body: some View {
         ZStack(alignment: Alignment.top) {
-            MapView(alert: $alert, nearByPlaces: $nearByPlaces, selectedCoordinate: $selectedCoordinate, selectedLocation: $newLocation)
+            MapView(alert: $alert, nearByPlaces: $nearByPlaces, selectedCoordinate: $draftSelectedCoordinate, selectedLocation: $draftNewLocation)
 
                 .alert(isPresented: $alert) {
                     Alert(title: Text("Please Enable Location Access In Settings Pannel !!!"))
                 }.edgesIgnoringSafeArea(.vertical)
 
             SlideOverCard(position: $cardPosition) {
-                SearchBarView(cardPosition: self.$cardPosition, nearByPlaces: self.$nearByPlaces, newLocation: self.$newLocation, selectedCoordinate: self.$selectedCoordinate).environmentObject(self.placeFinder).padding(.bottom, 5)
+                SearchBarView(cardPosition: self.$cardPosition, nearByPlaces: self.$nearByPlaces, newLocation: self.$draftNewLocation, selectedCoordinate: self.$draftSelectedCoordinate).environmentObject(self.placeFinder).padding(.bottom, 5)
             }.foregroundColor(.primary)
         }.edgesIgnoringSafeArea(.vertical)
+            .navigationBarItems(trailing: Button(action: {
+                self.newLocation = self.draftNewLocation
+                self.selectedCoordinate = self.draftSelectedCoordinate
+                self.mode.wrappedValue.dismiss()
+            }, label: { Text("Save") }))
 
 //    //            RecordsListView(newLocation: self.$newLocation)
 //    //                .environment(\.managedObjectContext, (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
