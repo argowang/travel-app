bb3fc4d115ae6b92eff452a265cea13ee8be381f
bxzhu <bingxinzhu95@gmail.com>
2019-12-02 18:38:46 -0800

    Add fake trip view (#40)
    * add new icons
    
    * add fake trip view

--- tracker/ContentView.swift
+++ tracker/ContentView.swift
@@ -13,7 +13,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            CardListView()
+            FakeTripView()
                 .navigationBarTitle("Your Trip")
         }
     }
