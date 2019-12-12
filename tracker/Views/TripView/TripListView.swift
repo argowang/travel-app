3df305725d189464b688079b54f3013efc259595
Nicole-gym <yg2529@columbia.edu>
2019-12-12 00:05:11 -0800

    copy grid view

--- tracker/Views/TripView/TripListView.swift
+++ tracker/Views/TripView/TripListView.swift
@@ -9,35 +9,70 @@
 import CoreData
 import SwiftUI
 
-struct TripListView: View {
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) { row in
+                HStack {
+                    ForEach(0 ..< self.columns) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
+struct subview: View {
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "MM-dd-yyyy HH:mm"
         return formatter
     }
-    
+
+    let row: Int
+    let col: Int
+    let index: Int
+    var tripCards: FetchedResults<TripCard>
+
+    var body: some View {
+        TripCardView(title: self.tripCards[index].title ?? "title place holder", dateString: self.dateFormatter.string(from: self.tripCards[index].start ?? Date()))
+    }
+
+    init(row: Int, col: Int, tripCards: FetchedResults<TripCard>) {
+        self.row = row
+        self.col = col
+        self.tripCards = tripCards
+        index = row * col + col
+    }
+}
+
+struct TripListView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     @Environment(\.editMode) var mode
     @FetchRequest(fetchRequest: TripCard.allTripCardsFetchRequest()) var tripCards: FetchedResults<TripCard>
     @State var title = ""
+    @State private var left = true
+    @State private var e = 1
     
     var body: some View {
             VStack {
                 ScrollView {
-                    ForEach(self.tripCards) { card in
-                            if self.mode?.wrappedValue == .active {
-                                Button("DELETE") {
-                                    self.managedObjectContext.delete(card)
-                                }
-                                .padding()
-
-                                TripCardView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))
-                            } else {
-                                NavigationLink(destination: TripInfoView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))) {
-                                    TripCardView(title: card.title ?? "title place holder", dateString: self.dateFormatter.string(from: card.start ?? Date()))
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                            }
+                    ForEach(0..<self.tripCards.count) { index in
+                        Text(String(describing: index)).tag(index)
+                    }
+                    GridStack(rows: tripCards.count / 2 + tripCards.count % 2, columns: 2) { row, col in
+                        subview(row: row, col: col, tripCards: self.tripCards)
                     }
                 }
                 VStack {
