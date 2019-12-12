4edbbfe3696adef9fd76b519fb521eefdaf03ee0
Nicole-gym <yg2529@columbia.edu>
2019-12-11 22:24:12 -0800

    tripview initial

---
+++ tracker/Views/TripView/TripCardView.swift
@@ -0,0 +1,50 @@
+//
+//  TripCardView.swift
+//  tracker
+//
+//  Created by 郭怡明 on 12/11/19.
+//  Copyright © 2019 TechLead. All rights reserved.
+//
+
+import SwiftUI
+
+struct TripCardView: View {
+    var title: String
+    var dateString: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(self.dateString)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(self.title)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                }
+                .layoutPriority(100)
+
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+        .frame(width: 150.0, height: 200.0)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 2.5)
+        )
+        .padding([.top, .horizontal])
+    }
+}
+
+struct TripCardView_Previews: PreviewProvider {
+    static var title = ""
+    static var dateString = ""
+    static var previews: some View {
+        TripCardView(title: title, dateString: dateString)
+    }
+}
---
+++ tracker/Views/TripView/TripInfoView.swift
@@ -0,0 +1,46 @@
+//
+//  TripInfoView.swift
+//  tracker
+//
+//  Created by 郭怡明 on 12/11/19.
+//  Copyright © 2019 TechLead. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct TripInfoView: View {
+    var title: String
+    var dateString: String
+
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trip Name : \(title)")
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+
+            HStack {
+                Text("Date is \(dateString)")
+            }
+
+            Spacer()
+        }
+    }
+}
+
+struct TripInfoView_Previews: PreviewProvider {
+    static var title = ""
+    static var dateString = ""
+    static var previews: some View {
+        TripInfoView(title: title, dateString: dateString)
+    }
+}
