7a198d79f331d2ef80f3b1657d07fe658f2c590a
Ergou <junyi.wang@columbia.edu>
2019-12-18 23:01:40 -0800

    extract save logic

--- tracker/Views/Event Views/AddEventViewV2.swift
+++ tracker/Views/Event Views/AddEventViewV2.swift
@@ -24,38 +24,8 @@ struct AddEventViewV2: View {
         }
         .navigationBarTitle(Text("\(draftEvent.type.rawValue)"), displayMode: .inline)
         .navigationBarItems(trailing: Button(action: {
-            var cardToSave: EventCard!
-            if self.draftEvent.event != nil {
-                cardToSave = self.draftEvent.event
-            } else {
-                cardToSave = EventCard(context: self.managedObjectContext)
-                cardToSave.uuid = UUID()
-                self.draftEvent.parentTrip.addToEvents(cardToSave)
-            }
-            cardToSave.placeName = self.draftEvent.place.name
-            cardToSave.latitude = self.draftEvent.place.coordinate?.latitude ?? 0
-            cardToSave.longitude = self.draftEvent.place.coordinate?.longitude ?? 0
-
-            if self.draftEvent.type == .transportation {
-                cardToSave.originName = self.draftEvent.origin.name
-                cardToSave.originLatitude = self.draftEvent.origin.coordinate?.latitude ?? 0
-                cardToSave.originLongitude = self.draftEvent.origin.coordinate?.longitude ?? 0
-                cardToSave.transportation = self.draftEvent.transportation
-            }
-
-            cardToSave.title = self.draftEvent.title
-
-            cardToSave.start = self.draftEvent.calculatedDate
-            cardToSave.type = self.draftEvent.type.rawValue
-            cardToSave.rating = Int16(self.draftEvent.rating)
-            cardToSave.price = self.draftEvent.price
-            cardToSave.eventDescription = self.draftEvent.eventDescription
-
-            do {
-                try self.managedObjectContext.save()
+            if self.draftEvent.saveToContext(self.managedObjectContext) {
                 self.mode.wrappedValue.dismiss()
-            } catch {
-                print(error)
             }
         }, label: { Text("Save") }) // .disabled(isSaveAllowed(draftEvent))
         )
